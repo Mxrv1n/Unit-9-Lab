3bc5384f962e7a9bdd27d9d729bcd071eaecc899
Marvin P <MarvinPerez1503@outlook.com>
2026-03-10 16:47:41 -0400

    class player init created

--- Lab9_mperez55.py
+++ Lab9_mperez55.py
@@ -3,5 +3,11 @@ Author: Marvin Perez
 Purpose: Match coins game lab 9.
 Date: 03/10/2026'''
 from coin import Coin
+from player import player
+
 coin1 = Coin()
 print(coin1.get_sideup())
+
+player = player(name="csa")
+#REMOVE LATER
+player.__printname__()
--- player.py
+++ player.py
@@ -2,3 +2,14 @@
 Author: Marvin Perez
 Purpose: Player file for Player class.
 Date: 03/10/2026'''
+from coin import Coin
+
+class player:
+    def __init__(self,name="Player 1"):
+        self.__name  = name
+        self.__wallet = 20
+        self.__coin = Coin() 
+
+    #REMOVE LATER
+    def __printname__(self):
+        print(self.__name)
